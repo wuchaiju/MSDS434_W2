a9ecb98e54ad984c41c6d0ef27c3b394754a9cf9
wuchaiju <wuchaiju@gmail.com>
2024-01-10 05:53:13 +0000

    comment

--- test2.py
+++ test2.py
@@ -5,6 +5,6 @@ PORT = 8002
 
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("ec2-35-89-201-160.us-west-2.compute.amazonaws.com", PORT), Handler) as httpd:
+witf socketserver.TCPServer(("MSDS434MSDS434_ModuleTwo", PORT), ler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
